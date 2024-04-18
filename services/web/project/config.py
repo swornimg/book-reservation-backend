54a27c0e27ddba04ce9caa56c1ac8bf34bdbe086
Surya Limbu <32124078@student.uwl.ac.uk>
2024-04-19 00:51:17 +0100

    sprint3: add test configuration and model test case

--- services/web/project/config.py
+++ services/web/project/config.py
@@ -10,3 +10,15 @@ class Config(object):
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
     MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
     SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+    WTF_CSRF_ENABLED = False
+    DEBUG = True
+    LOG_LEVEL = "DEBUG"
+    UPLOAD_FOLDER = f"{os.getenv('APP_FOLDER')}/project/uploads"
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+    
